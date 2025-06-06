526efba0ed68d06fd1528e20b4a30ea24d37dab7
Vikash Raj Paswan <vikashrajpaswan2000@gmail.com>
2025-06-06 18:12:45 +0530

    icons modified

---
+++ Frontend/risk-eval/src/components/dashboard/SeveritySummary.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import {
+  ShieldAlert,
+  ShieldCheck,
+  Shield,
+  ChartArea,
+  OctagonAlert,
+} from "lucide-react";
+import SeverityHighIcon from "@/assets/severity-high.svg";
+
+const stats = [
+  {
+    icon: (
+      <img
+        src={SeverityHighIcon}
+        alt="High Priority"
+        className="h-10 w-10 mr-4"
+      />
+    ),
+    count: 10,
+    label: "High Severity",
+  },
+  {
+    icon: <OctagonAlert className="h-12 w-12 text-yellow-500" />,
+    count: 20,
+    label: "Medium Severity",
+  },
+  {
+    icon: <OctagonAlert className="h-12 w-12 text-green-500" />,
+    count: 30,
+    label: "Low Severity",
+  },
+];
+
+const SeveritySummary = () => {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-3 gap-6">
+      {stats.map((stat, index) => (
+        <div
+          key={index}
+          className="bg-white shadow-md rounded-lg p-6 flex items-center"
+        >
+          <div className="mr-4">{stat.icon}</div>
+
+          <div>
+            <div className="text-2xl font-bold">{stat.count}</div>
+            <div className="text-gray-600">{stat.label}</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default SeveritySummary;
--- Frontend/risk-eval/src/pages/Dashboard/Dashboard.jsx
+++ Frontend/risk-eval/src/pages/Dashboard/Dashboard.jsx
@@ -3,45 +3,12 @@ import { ChartPieInteractive } from "@/components/dashboard/ChartPieDonutInterac
 import { ChartPieDonutText } from "@/components/dashboard/ChartPieDonutText";
 import { ChartRadialLabel } from "@/components/dashboard/ChartRadial";
 import { DataTableDemo } from "@/components/dashboard/DataTableDemo";
-import { ShieldAlert, ShieldCheck, Shield, ChartArea } from "lucide-react";
-// import SeverityHighIcon from "@/assets/";
-
-const stats = [
-  {
-    icon: <ShieldAlert className="h-12 w-12 text-red-500" />,
-    count: 10,
-    label: "High Severity",
-  },
-  {
-    icon: <Shield className="h-12 w-12 text-yellow-500" />,
-    count: 20,
-    label: "Medium Severity",
-  },
-  {
-    icon: <ShieldCheck className="h-12 w-12 text-green-500" />,
-    count: 30,
-    label: "Low Severity",
-  },
-];
+import SeveritySummary from "@/components/dashboard/SeveritySummary";
 
 export default function DashboardCards() {
   return (
     <div className="bg-gray-100 p-6">
-      <div className="grid grid-cols-1 sm:grid-cols-3 gap-6">
-        {stats.map((stat, index) => (
-          <div
-            key={index}
-            className="bg-white shadow-md rounded-lg p-6 flex items-center"
-          >
-            <div className="mr-4">{stat.icon}</div>
-
-            <div>
-              <div className="text-2xl font-bold">{stat.count}</div>
-              <div className="text-gray-600">{stat.label}</div>
-            </div>
-          </div>
-        ))}
-      </div>
+      <SeveritySummary />
 
       <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 mt-6">
         <ChartPieDonutText />
